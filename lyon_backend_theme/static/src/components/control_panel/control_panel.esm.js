e8910a868f7f70d024d12abf803a26eb7a63a272
khaledhathout <khkhaledhathout@gmail.com>
2022-08-25 17:35:04 +0300

    20220825V1

---
+++ lyon_backend_theme/static/src/components/control_panel/control_panel.esm.js
@@ -0,0 +1,43 @@
+/** @odoo-module **/
+
+import LegacyControlPanel from "web.ControlPanel";
+import {ControlPanel} from "@web/search/control_panel/control_panel";
+import {SearchBar} from "@web/search/search_bar/search_bar";
+import {deviceContext} from "@lyon_backend_theme/components/ui_context.esm";
+import {patch} from "web.utils";
+
+const {useState, useContext} = owl.hooks;
+
+// In v15.0 there are two ControlPanel's. They are mostly the same and are used in legacy and new owl views.
+// We extend them two mostly the same way.
+
+// Patch legacy control panel to add states for mobile quick search
+patch(LegacyControlPanel.prototype, "lyon_backend_theme.LegacyControlPanelMobile", {
+    setup() {
+        this._super();
+        this.state = useState({
+            mobileSearchMode: this.props.withBreadcrumbs ? "" : "quick",
+        });
+        this.ui = useContext(deviceContext);
+    },
+    setMobileSearchMode(ev) {
+        this.state.mobileSearchMode = ev.detail;
+    },
+});
+
+// Patch control panel to add states for mobile quick search
+patch(ControlPanel.prototype, "lyon_backend_theme.ControlPanelMobile", {
+    setup() {
+        this._super();
+        this.state = useState({
+            mobileSearchMode: "",
+        });
+        this.ui = useContext(deviceContext);
+    },
+    setMobileSearchMode(ev) {
+        this.state.mobileSearchMode = ev.detail;
+    },
+});
+patch(SearchBar, "lyon_backend_theme.SearchBarMobile", {
+    template: "lyon_backend_theme.SearchBar",
+});
